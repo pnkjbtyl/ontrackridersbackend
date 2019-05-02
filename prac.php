0d68a60b274a0f574da9299d150d49cec75ba1d8
dipshi01 <50194437+dipshi01@users.noreply.github.com>
2019-05-02 15:19:12 +0530

    Third commit from Dipshi

--- prac.php
+++ prac.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1>Dipshi</h1>
+<h1>File</h1>
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </form>
-
 </body>
 </html> 	
