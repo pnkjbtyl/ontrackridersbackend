f1ad06164a81bfa59ebfb2460900bedb5af0304f
Pankaj Batyal <pnkj.btyl@gmail.com>
2019-05-03 16:58:58 +0530

    Updated changes

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
-
+	
 	$routes['default_controller'] = 'home';
 	$routes['default_function'] = 'welcome';
 
 
 
 	$current_controller = $routes['default_controller'].'Controller';
-    $current_function = $routes['default_function'];
+   	$current_function = $routes['default_function'];
     
 	require_once('application/controller/'.$current_controller.'.php');
 	$current_controller = new $current_controller;
 	$current_controller->{$current_function}();
+	
 ?>
